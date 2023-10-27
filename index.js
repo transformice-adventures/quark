8443a5fa188576df173f38b3af8f02b5a91e2830
Hadaward <eduarod.gimenez07@gmail.com>
2023-10-27 08:36:34 -0400

    test auto updater

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('test');
+'use strict';
+
+async function loadManifest(uri) {
+    const manifestRequest = await fetch(uri);
+    return await manifestRequest.json();
+}
+
+(async function() {
+    /**
+     * Auto updater
+     */
+    const { version } = await loadManifest('./manifest.json');
+    const onlineManifest = await loadManifest(`https://github.com/transformice-adventures/quark/manifest.json?d=${Date.now()}`);
+
+    if (version.major !== onlineManifest.version.major || version.minor !== onlineManifest.version.minor || version.build !== onlineManifest.version.build) {
+        const quarkIndexRequest = await fetch(`https://github.com/transformice-adventures/quark/index.js?d=${Date.now()}`);
+        const quarkIndex = await quarkIndexRequest.arrayBuffer();
+
+        const { writeFile } = require('fs/promises');
+        
+        await writeFile('./index.js', quarkIndex);
+    }
+    /**
+     * EOF Auto Updater
+     */
+})();
